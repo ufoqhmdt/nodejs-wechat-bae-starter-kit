92678075e75f5f4ccc0d245e4a14d0048b01eba2
HongminQiao[UFO] <qiaohongmin082@pingan.com.cn>
2016-01-01 22:49:29 +0800

    update
    Signed-off-by: HongminQiao[UFO] <qiaohongmin082@pingan.com.cn>

--- server.js
+++ server.js
@@ -8,15 +8,61 @@
 
 
 var express = require('express');
+var crypto = require("crypto");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World000!');
+function valid(req, res) {
+    var TOKEN = 'weixin';
+    var signature = req.params.signature;
+    var timestamp = req.params.timestamp;
+    var nonce = req.params.nonce;
+    var token = TOKEN;
+    if (isLegel(signature, timestamp, nonce, token)) {
+        res.send('success');
+    } else {
+        res.send('fail');
+    }
+}
+
+function isLegel(signature, timestamp, nonce, token) {
+    var array = new Array();
+    array[0] = timestamp;
+    array[1] = nonce;
+    array[2] = token;
+    array.sort();
+    var hasher = crypto.createHash("sha1");
+    var msg = array[0] + array[1] + array[2];
+    hasher.update(msg);
+    var msg = hasher.digest('h<span></span>ex'); //计算SHA1值   
+    res.send(msg);
+    if (msg == signature) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+app.get('/', function(req, res) {
+    valid(req, res);
+
 });
 
-var server = app.listen(18080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var wechat = require('wechat');
+var config = {
+    token: 'weixin',
+    appid: 'wx870eb5ff3f3c932f',
+    encodingAESKey: 'R4fdRXMHzN4MdJtTtAmXgYULP9dv41be9Ns9oGfzyJS'
+};
 
-  console.log('Example app listening at http://%s:%s', host, port);
+app.use(express.query());
+
+app.use('/wechat', wechat(config, function(req, res, next) { // 微信输入信息都在req.weixin上   var message = req.weixin;   if (message.FromUserName === 'diaosi') {   	// 回复屌丝(普通回复)   	res.reply('hehe');   } else if (message.FromUserName === 'text') {   	//你也可以这样回复text类型的信息   	res.reply({   		content: 'text object',   		type: 'text'   	});   } else if (message.FromUserName === 'hehe') {   	// 回复一段音乐   	res.reply({   		type: "music",   		content: {   			title: "来段音乐吧",   			description: "一无所有",   			musicUrl: "http://mp3.com/xx.mp3",   			hqMusicUrl: "http://mp3.com/xx.mp3",   			thumbMediaId: "thisThumbMediaId"   		}   	});   } else {   	// 回复高富帅(图文回复)   	res.reply([{   		title: '你来我家接我吧',   		description: '这是女神与高富帅之间的对话',   		picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',   		url: 'http://nodeapi.cloudfoundry.com/'   	}]);   }
+}));
+
+
+
+var server = app.listen(18080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
 });
