a63d31a45550687cb786a528a7e98ad0f11f1b5b
HongminQiao[UFO] <qiaohongmin082@pingan.com.cn>
2016-01-01 20:59:42 +0800

    express wechat
    Signed-off-by: HongminQiao[UFO] <qiaohongmin082@pingan.com.cn>

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-var http = require('http');
-var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
-}).listen(port);
+// var http = require('http');
+// var port = 18080;
+// http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('<h1>Node.js</h1>');
+//     res.end('<p>Hello World</p>');
+// }).listen(port);
+
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
