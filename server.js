c3ab11da84ff9f2ef91f09b69c66ff31e0dd0c99
HongminQiao[UFO] <qiaohongmin082@pingan.com.cn>
2016-01-02 12:17:24 +0800

    frank update
    Signed-off-by: HongminQiao[UFO] <qiaohongmin082@pingan.com.cn>

--- server.js
+++ server.js
@@ -3,71 +3,34 @@
 // http.createServer(function(req, res) {
 //     res.writeHead(200, {'Content-Type': 'text/html'});
 //     res.write('<h1>Node.js</h1>');
-//     res.end('<p>Hello World</p>');
+//     res.end('<p>Hello World</p >');
 // }).listen(port);
 
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var crypto = require("crypto");
 var app = express();
 
-function valid(req, res) {
-    var TOKEN = 'weixin';
-    var signature = req.params.signature;
-    var timestamp = req.params.timestamp;
-    var nonce = req.params.nonce;
-    var echostr = req.params.echostr;
-    var token = TOKEN;
-    // res.send(isLegel(signature, timestamp, nonce, token));
-    if (isLegel(signature, timestamp, nonce, token)) {
-        res.send(echostr);
-    } else {
-        res.send('fail');
-    }
-}
-
-function isLegel(signature, timestamp, nonce, token) {
-    var array = new Array();
-    array[0] = timestamp;
-    array[1] = nonce;
-    array[2] = token;
-    array.sort();
-    var hasher = crypto.createHash("sha1");
-    var msg = array[0] + array[1] + array[2];
-    hasher.update(msg);
-    var msg = hasher.digest('h<span></span>ex'); //计算SHA1值   
-    // return [msg,signature];
-    if (msg == signature) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-app.get('/', function(req, res) {
-    valid(req, res);
-
-});
-
 var wechat = require('wechat');
+
+app.use(bodyParser());
 var config = {
     token: 'weixin',
     appid: 'wx870eb5ff3f3c932f',
     encodingAESKey: 'R4fdRXMHzN4MdJtTtAmXgYULP9dv41be9Ns9oGfzyJS'
 };
 
-app.use(express.query());
-
-app.use('/wechat', wechat(config, function(req, res, next) { // 微信输入信息都在req.weixin上   
+app.use('/wechat', wechat(config, function(req, res, next) { // 微信输入信息都在req.weixin上
     var message = req.weixin;
-    if (message.FromUserName === 'diaosi') { // 回复屌丝(普通回复)   	
+    if (message.FromUserName === 'diaosi') { // 回复屌丝(普通回复)
         res.reply('hehe');
-    } else if (message.FromUserName === 'text') { //你也可以这样回复text类型的信息   	
+    } else if (message.FromUserName === 'text') { //你也可以这样回复text类型的信息
         res.reply({
             content: 'text object',
             type: 'text'
         });
-    } else if (message.FromUserName === 'hehe') { // 回复一段音乐   	
+    } else if (message.FromUserName === 'hehe') { // 回复一段音乐
         res.reply({
             type: "music",
             content: {
@@ -78,7 +41,7 @@ app.use('/wechat', wechat(config, function(req, res, next) { // 微信输入信�
                 thumbMediaId: "thisThumbMediaId"
             }
         });
-    } else { // 回复高富帅(图文回复)   	
+    } else { // 回复高富帅(图文回复)
         res.reply([{
             title: '你来我家接我吧',
             description: '这是女神与高富帅之间的对话',
